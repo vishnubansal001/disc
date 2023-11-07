b11a722e791e68929c34504d676b50a6ef72a5da
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-11-07 14:30:05 +0530

    functionality for leave server

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -7,7 +7,7 @@ import { InviteModal } from "@/components/modals/invite-modal";
 import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { MembersModal } from "@/components/modals/members-modal";
 import { CreateChannelModal } from "@/components/modals/create-channel-modal";
-// import { LeaveServerModal } from "@/components/modals/leave-server-modal";
+import { LeaveServerModal } from "@/components/modals/leave-server-modal";
 // import { DeleteServerModal } from "@/components/modals/delete-server-modal";
 // import { DeleteChannelModal } from "@/components/modals/delete-channel-modal";
 // import { EditChannelModal } from "@/components/modals/edit-channel-modal";
@@ -32,8 +32,8 @@ export const ModalProvider = () => {
       <EditServerModal />
       <MembersModal />
       <CreateChannelModal />
-      {/* <LeaveServerModal />
-      <DeleteServerModal />
+      <LeaveServerModal />
+      {/*<DeleteServerModal />
       <DeleteChannelModal />
       <EditChannelModal />
       <MessageFileModal />
