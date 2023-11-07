92ebf78347b964b94099293ed2ffd098fa033a1c
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-11-07 14:52:17 +0530

    edit and delete channle

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -9,8 +9,8 @@ import { MembersModal } from "@/components/modals/members-modal";
 import { CreateChannelModal } from "@/components/modals/create-channel-modal";
 import { LeaveServerModal } from "@/components/modals/leave-server-modal";
 import { DeleteServerModal } from "@/components/modals/delete-server-modal";
-// import { DeleteChannelModal } from "@/components/modals/delete-channel-modal";
-// import { EditChannelModal } from "@/components/modals/edit-channel-modal";
+import { DeleteChannelModal } from "@/components/modals/delete-channel-modal";
+import { EditChannelModal } from "@/components/modals/edit-channel-modal";
 // import { MessageFileModal } from "@/components/modals/message-file-modal";
 // import { DeleteMessageModal } from "@/components/modals/delete-message-modal";
 
@@ -34,9 +34,9 @@ export const ModalProvider = () => {
       <CreateChannelModal />
       <LeaveServerModal />
       <DeleteServerModal />
-      {/*<DeleteChannelModal />
+      <DeleteChannelModal />
       <EditChannelModal />
-      <MessageFileModal />
+      {/*<MessageFileModal />
       <DeleteMessageModal /> */}
     </>
   );
