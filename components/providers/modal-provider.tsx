41f21fb9965692f8c97c14a369d4d157ad233a8d
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-11-07 13:06:07 +0530

    new:added the functionality

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 import { EditServerModal } from "@/components/modals/edit-server-modal";
 import { InviteModal } from "@/components/modals/invite-modal";
 import { CreateServerModal } from "@/components/modals/create-server-modal";
-// import { MembersModal } from "@/components/modals/members-modal";
+import { MembersModal } from "@/components/modals/members-modal";
 // import { CreateChannelModal } from "@/components/modals/create-channel-modal";
 // import { LeaveServerModal } from "@/components/modals/leave-server-modal";
 // import { DeleteServerModal } from "@/components/modals/delete-server-modal";
@@ -30,8 +30,8 @@ export const ModalProvider = () => {
       <CreateServerModal />
       <InviteModal />
        <EditServerModal />
-      {/*<MembersModal />
-      <CreateChannelModal />
+      <MembersModal />
+     {/* <CreateChannelModal />
       <LeaveServerModal />
       <DeleteServerModal />
       <DeleteChannelModal />
