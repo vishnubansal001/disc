6b9b2166cb197cafdc7499a08a7ec0ed46b20987
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-11-07 12:43:29 +0530

    new:link generation routes added

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 
 // import { EditServerModal } from "@/components/modals/edit-server-modal";
-// import { InviteModal } from "@/components/modals/invite-modal";
+import { InviteModal } from "@/components/modals/invite-modal";
 import { CreateServerModal } from "@/components/modals/create-server-modal";
 // import { MembersModal } from "@/components/modals/members-modal";
 // import { CreateChannelModal } from "@/components/modals/create-channel-modal";
@@ -28,8 +28,8 @@ export const ModalProvider = () => {
   return (
     <>
       <CreateServerModal />
-      {/* <InviteModal />
-      <EditServerModal />
+      <InviteModal />
+      {/* <EditServerModal />
       <MembersModal />
       <CreateChannelModal />
       <LeaveServerModal />
