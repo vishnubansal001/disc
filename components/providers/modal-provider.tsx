25fac2ebb9645d034e4ae99833b357ed4885fd80
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-11-07 14:32:04 +0530

    function for delting

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -8,7 +8,7 @@ import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { MembersModal } from "@/components/modals/members-modal";
 import { CreateChannelModal } from "@/components/modals/create-channel-modal";
 import { LeaveServerModal } from "@/components/modals/leave-server-modal";
-// import { DeleteServerModal } from "@/components/modals/delete-server-modal";
+import { DeleteServerModal } from "@/components/modals/delete-server-modal";
 // import { DeleteChannelModal } from "@/components/modals/delete-channel-modal";
 // import { EditChannelModal } from "@/components/modals/edit-channel-modal";
 // import { MessageFileModal } from "@/components/modals/message-file-modal";
@@ -33,8 +33,8 @@ export const ModalProvider = () => {
       <MembersModal />
       <CreateChannelModal />
       <LeaveServerModal />
-      {/*<DeleteServerModal />
-      <DeleteChannelModal />
+      <DeleteServerModal />
+      {/*<DeleteChannelModal />
       <EditChannelModal />
       <MessageFileModal />
       <DeleteMessageModal /> */}
